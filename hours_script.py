b5225db298f4487da8407d83c48be4269a056d26
kurtjthompson <thompsonkurt@gmail.com>
2017-08-07 09:13:37 -0500

    Interface

---
+++ hours_script.py
@@ -0,0 +1,16 @@
+monday_am = float(input('What time did you arrive on Monday?'))
+monday_pm = float(input('What time did you leave on Monday?'))
+monday = 12-monday_am + monday_pm
+tuesday_am = float(input('What time did you arrive on Tuesday?'))
+tuesday_pm = float(input('What time did you leave on Tuesday?'))
+tuesday = 12-tuesday_am + tuesday_pm
+wednesday_am = float(input('What time did you arrive on Wednesday?'))
+wednesday_pm = float(input('What time did you leave on Wednesday?'))
+wednesday = 12-wednesday_am + wednesday_pm
+thursday_am = float(input('What time did you arrive on Thursday?'))
+thursday_pm = float(input('What time did you leave on Thursday?'))
+thursday = 12-thursday_am + thursday_pm
+friday_am = float(input('What time did you arrive on Friday?'))
+friday_pm = float(input('What time did you leave on Friday?'))
+friday = 12-friday_am + friday_pm
+print('Monday ', monday, ', Tuesday ', tuesday, ', Wednesday ', wednesday, ', Thursday ', thursday, ', Friday', friday)

472dc33c5d382d62e4b567d6a96fc25ab659a8d8
kurtjthompson <thompsonkurt@gmail.com>
2017-08-07 09:25:50 -0500

    add_lunch

--- hours_script.py
+++ hours_script.py
@@ -1,16 +1,21 @@
 monday_am = float(input('What time did you arrive on Monday?'))
 monday_pm = float(input('What time did you leave on Monday?'))
-monday = 12-monday_am + monday_pm
+monday_lunch = float(input('Did you take any time off for lunch?'))
+mon = 12-monday_am + monday_pm - monday_lunch
 tuesday_am = float(input('What time did you arrive on Tuesday?'))
 tuesday_pm = float(input('What time did you leave on Tuesday?'))
-tuesday = 12-tuesday_am + tuesday_pm
+tuesday_lunch = float(input('Did you take any time off for lunch?'))
+tue = 12-tuesday_am + tuesday_pm - tuesday_lunch
 wednesday_am = float(input('What time did you arrive on Wednesday?'))
 wednesday_pm = float(input('What time did you leave on Wednesday?'))
-wednesday = 12-wednesday_am + wednesday_pm
+wednesday_lunch = float(input('Did you take any time off for lunch?'))
+wed = 12-wednesday_am + wednesday_pm - wednesday_lunch
 thursday_am = float(input('What time did you arrive on Thursday?'))
 thursday_pm = float(input('What time did you leave on Thursday?'))
-thursday = 12-thursday_am + thursday_pm
+thursday_lunch = float(input('Did you take any time off for lunch?'))
+thur = 12-thursday_am + thursday_pm - thursday_lunch
 friday_am = float(input('What time did you arrive on Friday?'))
 friday_pm = float(input('What time did you leave on Friday?'))
-friday = 12-friday_am + friday_pm
-print('Monday ', monday, ', Tuesday ', tuesday, ', Wednesday ', wednesday, ', Thursday ', thursday, ', Friday', friday)
+friday_lunch = float(input('Did you take any time off for lunch?'))
+fri = 12-friday_am + friday_pm - friday_lunch
+print('Monday ', mon, ', Tuesday ', tue, ', Wednesday ', wed, ', Thursday ', thur, ', Friday', fri)
